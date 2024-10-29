3d47e954b262c3e4bddb7b274dff5725e517fbad
salim-zemmouk <158276033+salim-zemmouk@users.noreply.github.com>
2024-10-29 15:38:12 +0100

    Ajout des test api plus test ihm authentification

--- cypress.config.js
+++ cypress.config.js
@@ -1,40 +1,9 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
-
-      require('cypress-mochawesome-reporter/plugin')(on);
-
-      on("before:browser:launch", (browser, launchOptions) => {
-        console.log(launchOptions.args);
-        if (browser.name === "chrome") {
-          launchOptions.args.push("--incognito");
-        }
-        return launchOptions;
-      });
-
-      on("task", {
-        async connectDB(query){
-          const client = new Client({
-            user: "postgres",
-            password: "staff-manager",
-            host: "database-1.chqar689pasc.us-east-2.rds.amazonaws.com",
-            database: "postgres",
-            ssl: false,
-            port: 5432
-          })
-
-          const clientDB = new pg.Pool(dbConfig)
-          //return thw result from sql
-          return clientDB.query(sql)
-          // await client.connect()
-          // const res = await client.query(query)
-          // await client.end()
-          // return res.rows;
-        }
-      })
+      // implement node event listeners here
     },
   },
 });
--- cypress/e2e/scpi-invest-api.cy.js
+++ cypress/e2e/scpi-invest-api.cy.js
@@ -14,4 +14,127 @@ describe("it should test SCPI INVEST APIs", () => {
         });
     });
 
+
+    // Récupérer les détails d'une SCPI par ID
+    it('should tests API Recuperate SCPI Par ID', () => {
+        cy.getScpiInvestToken().then((token) => {
+            cy.request({
+                method: 'GET',
+                url: 'https://qua.scpi-invest-api.check-consulting.net/api/v1/scpi/2',
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+
+            }).then((response) => {
+                expect(response.status).to.eq(200);
+                console.log('Response de l\'API:', response.body);
+            });
+        });
+
+    });
+
+
+    //Test API filtrer les SCPI
+    it('Should tests API filter recherche ', () => {
+        cy.getScpiInvestToken().then((token) => {
+            cy.request({
+                method: 'POST',
+                url: 'https://qua.scpi-invest-api.check-consulting.net/api/v1/scpi/search',
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+                body: {
+                        "searchTerm": "string",
+                        "localizations": [
+                            "France"
+                        ],
+                        "sectors": [
+                            "Bureaux",
+                            "Commerces"
+                        ],
+                        "amount": 1006,
+                        "fees": true
+                    }
+
+            }).then((response) => {
+                expect(response.status).to.eq(200);
+                console.log('Response de l\'API:', response.body);
+            });
+        });
+
+    });
+
+
+
+    //Test API endorsement programmé
+
+    it('should tests API unified investment', () => {
+            cy.getScpiInvestToken().then((token) => {
+                cy.request({
+                    method: 'POST',
+                    url: 'https://qua.scpi-invest-api.check-consulting.net/api/v1/planified-investement',
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                    body: {
+                        "frequency":"MENSUELLE",
+                        "amount":20000,
+                        "debitDayOfMonth":10,
+                        "numberOfShares":1600,
+                        "scpi":1
+                    }
+
+                }).then((response) => {
+                    expect(response.status).to.eq(201);
+                    console.log('Response de l\'API:', response.body);
+                });
+            });
+    });
+
+    //Test API Comparator
+
+    it('should tests API Compare', () => {
+            cy.getScpiInvestToken().then((token) => {
+              cy.request({
+                  method: 'POST',
+                  url: 'https://qua.scpi-invest-api.check-consulting.net/api/v1/scpi/compare',
+                  headers: {
+                      Authorization: `Bearer ${token}`,
+                  },
+                  body:
+                      {
+                          "selectedScpis": [
+                              "Activimmo",
+                              "Altixia Cadence XII",
+                              "Atream Hotels"
+                          ],
+                          "investValue": 1000000
+                      }
+
+              }).then((response) => {
+                  expect(response.status).to.eq(200);
+                  console.log('Response de l\'API:', response.body);
+              });
+            });
+    });
+
+    //Test API recuperate investment
+
+    it('should tests API investment recuperate', () => {
+        cy.getScpiInvestToken().then((token) => {
+            cy.request({
+                method: 'GET',
+                url: 'https://qua.scpi-invest-api.check-consulting.net/api/v1/investement',
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+
+            }).then((response) => {
+                expect(response.status).to.eq(200);
+                console.log('Response de l\'API:', response.body);
+                cy.log(JSON.stringify(response.body)); // Affiche le corps de la réponse
+                console.log(response.body); // Affiche dans la console du navigateur
+            });
+        });
+    });
 })
